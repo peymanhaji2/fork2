038df6bfbccbe66af5681840f6dad5541d2d4cd5
peymanhaji <71074308+peymanhaji@users.noreply.github.com>
2024-01-27 19:28:21 +0330

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,22 @@
+// https://family.cloudflare-dns.com/dns-query
+const redirect_doh = 'https://family.dot.dns.yandex.net/dns-query';
+const authPath = '/12345678';
+
+
+
+export default {
+    async fetch(request) {
+        const parsed_url = new URL(request.url);
+        if (parsed_url.pathname !== authPath) {
+            return;
+        }
+        else {
+            return fetch(redirect_doh + parsed_url.search, {
+                method: request.method,
+                headers: request.headers,
+                body: request.body,
+                redirect: request.redirect
+            });
+        } 
+    },
+};
